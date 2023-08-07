493b38eefcec97293df43d02ece7a4b28a2ce523
brux1999 <bvidal2018@gmail.com>
2023-08-07 19:11:55 -0300

    Remover atributo del boton

--- eventos.js
+++ eventos.js
@@ -1,13 +1,16 @@
  document.addEventListener('DOMContentLoaded', function() {
  const miBoton = document.querySelector("button"); 
- 
+
  //Utilizo es querySelector (el cual selecciona un elemento segun su etiqueta en css) 
  // porque al usar el getElementByClass me devuelve un array.
 
  miBoton.addEventListener('click', function() {
      alert('Hola! Soy el div');
      });
+     miBoton.removeAttribute("onclick"); 
+     // En este caso sirve tambien se podria utilizar stopPropagation()
  });
+
 
 // document.addEventListener('DOMContentLoaded', function() {
 //     const container = document.getElementsByClassName("Saludo");
