563bb0821b3962d8fd5d82fdfe6edd77c5e6efa7
brux1999 <bvidal2018@gmail.com>
2023-08-07 18:51:45 -0300

    Se agrega evento

---
+++ eventos.js
@@ -0,0 +1,22 @@
+ document.addEventListener('DOMContentLoaded', function() {
+ const miBoton = document.querySelector("button"); 
+ 
+ //Utilizo es querySelector (el cual selecciona un elemento segun su etiqueta en css) 
+ // porque al usar el getElementByClass me devuelve un array.
+
+ miBoton.addEventListener('click', function() {
+     alert('Hola! Soy el div');
+     });
+ });
+
+// document.addEventListener('DOMContentLoaded', function() {
+//     const container = document.getElementsByClassName("Saludo");
+//     const miBoton = container[0];
+
+//       miBoton.addEventListener('click', function() {
+//         alert('Hola! Soy el div');
+//       });
+//     });
+
+//Esta es otra opcion que encontre si si quiere utilizar getElement, 
+//seleccionar elemento 0 del array
